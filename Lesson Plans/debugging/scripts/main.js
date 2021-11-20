a8a010faefcca23d08aafb875dd38ec1148d7180
Omar Nema <omarwnema@gmail.com>
2021-11-20 16:12:17 -0500

    new workshop

--- Lesson Plans/debugging/scripts/main.js
+++ Lesson Plans/debugging/scripts/main.js
@@ -1,18 +1,27 @@
-const users = 
-[
- {name: 'ahmed', age: 25},
- {name: 'maria', age: 25},
- {name: 'carlos', age: 25},
- {name: 'noor'},
- {name: 'steph', age: 25}
- {name: 'kevin', age: 25}
-]
+const names = [
+  { name: 'ahmed' },
+  { name: 'maria' },
+  { name: 'carlos' },
+  { name: 'noor' }
+  { name: 'steph' },
+  { name: 'kevin' }
+];
 
-//make users array much bigger
-// for (i=0; i < 1000; i++){
-//   users.append({name: 'baby!!', age: 2})
-// }
+function addUserDivs() {
+  names.forEach((d) => {
+    let el = document.createElement('li');
+    el.innerText = `Name: ${d.name}`;
+    document.querySelector('name-list').append(el);
+  });
+}
 
-users.forEach(d=> {
-  console.log(d);
-})
+function listForInput() {
+  document.querySelector('#addName').addEventListener('click', () => {
+    let currentInputVal = '';
+    let el = document.createElement('li');
+    el.innerText = currentInputVal;
+    document.querySelector('.name-list').append(el);
+  });
+}
+
+addUserDivs();
